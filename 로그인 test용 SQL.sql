9c2fa0e407fc27587749acaaea32856c095b8f4b
Dayeon-Merry <131240376+Dayeon-Merry@users.noreply.github.com>
2023-08-22 22:19:10 +0900

    Delete 로그인 test용 SQL.sql

--- 로그인 test용 SQL.sql
+++
@@ -1,9 +0,0 @@
-create database myapp;
-use myapp;
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(50),
-  password VARCHAR(50)
-);
-INSERT INTO users (username, password)
-VALUES ('root', '1234');
